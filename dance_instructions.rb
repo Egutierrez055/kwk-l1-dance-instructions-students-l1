210a5328a62248347f4972ed4f1130405d427ad6
Egutierrez055 <egutierrez055@gmail.com>
2018-06-04 22:10:11 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -23,5 +23,11 @@ end
 def Bob_the_Reins
   puts "Holding your arms out infront of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end
+
+def lasso
+  puts : "Starts arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift your right arm up and point right elbow diagonally to the right."
   puts ""
   
