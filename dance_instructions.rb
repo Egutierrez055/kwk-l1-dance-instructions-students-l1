3529ea6d13e45443146e9f7ce604ca692a45e6c4
Egutierrez055 <egutierrez055@gmail.com>
2018-06-04 22:07:46 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -5,4 +5,23 @@ end
 def base_footwork
   puts "Lift right foot."
   puts "Return right foot to the ground"
-  puts "Finishing with a smallskip-step backward"
+  puts "Finishing with a small skip-step backward"
+end
+
+def skip_step_left
+  puts "Lower left foot to ground"
+  puts "Bounce left foot back up slighlty, kicking it a few inches back"
+  puts "Left, right, left and left"
+end
+
+def skip_step_right
+  puts "Lower right foot to ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Right left right and right"
+end
+
+def Bob_the_Reins
+  puts "Holding your arms out infront of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts ""
+  
