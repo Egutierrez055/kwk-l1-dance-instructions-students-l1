ac04c63fed680108acfd723b683697144b1203bd
Egutierrez055 <egutierrez055@gmail.com>
2018-06-04 22:31:07 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,110 @@
+def proper_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose."
+end
 
-# Code your methods below
+def base_footwork
+  puts "Lift right foot."
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
+end
+
+def skip_step_left
+  puts "Lower left foot to ground"
+  puts "Bounce left foot back up slighlty, kicking it a few inches back"
+  puts "Left, right, left and left"
+end
+
+def skip_step_right
+  puts "Lower right foot to ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Right left right and right"
+end
+
+def bob_the_reins
+  puts "Holding your arms out infront of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end
+
+def lasso
+  puts : "Starts arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift your right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+
+def bust_a_move
+  puts "Be cool"
+  puts "Be snazzy"
+end
+
+proper_stance
+bust_a_move
+base_footwork
+bob_the_reins
+puts "while also"
+skip_step_right
+skip_step_left
+bob_the_reins 
+puts "while also"
+skip_step_right
+skip_step_left
+bob_the_reins 
+puts "while also"
+skip_step_right
+skip_step_left
+bob_the_reins
+puts "while also"
+skip_step_right
+skip_step_left
+bob_the_reins
+puts "while also"
+skip_step_right
+skip_step_left
+bob_the_reins 
+puts "while also"
+skip_step_right
+skip_step_left
+bob_the_reins 
+puts "while also"
+skip_step_right
+skip_step_left
+bob_the_reins 
+puts "while also"
+skip_step_right
+skip_step_left
+puts "all at the same time." 
+puts "Your arm movements and steps should match up."
+puts "THEN"
+lasso 
+puts "while also"
+skip_step_right
+skip_step_left
+lasso 
+puts "while also"
+skip_step_right
+skip_step_left
+lasso 
+puts "while also"
+skip_step_right
+skip_step_left
+lasso 
+puts "while also"
+skip_step_right
+skip_step_left
+lasso 
+puts "while also"
+skip_step_right
+skip_step_left
+lasso 
+puts "while also"
+skip_step_right
+skip_step_left
+lasso 
+puts "while also"
+skip_step_right
+skip_step_left
+lasso 
+puts "while also"
+skip_step_right
+skip_step_left
+
